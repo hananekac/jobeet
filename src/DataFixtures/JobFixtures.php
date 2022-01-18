e796a92e8d7078daa84f5132563c72ae5dae8841
KACEMI <hanane.kacemi@capgemini.com>
2022-01-18 13:00:20 +0100

    add faker bundle for dummy data

--- src/DataFixtures/JobFixtures.php
+++ src/DataFixtures/JobFixtures.php
@@ -6,53 +6,48 @@ use App\Entity\Job;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class JobFixtures extends Fixture implements DependentFixtureInterface
 {
+    const JOB_TYPE = [
+      'full-time',
+      'part-time'
+    ];
     public function load(ObjectManager $manager): void
     {
-        $jobSensioLabs = new Job();
-        $jobSensioLabs->setCategory($this->getReference(CategoryFixtures::CATEGORY_PROGRAMMING));
-        $jobSensioLabs->setType('full-time');
-        $jobSensioLabs->setCompany('Sensio Labs');
-        $jobSensioLabs->setLogo('sensio-labs.gif');
-        $jobSensioLabs->setUrl('http://www.sensiolabs.com/');
-        $jobSensioLabs->setPosition('Web Developer');
-        $jobSensioLabs->setLocation('Paris, France');
-        $jobSensioLabs->setDescription('You\'ve already developed websites with symfony and you want to work with Open-Source technologies. You have a minimum of 3 years experience in web development with PHP or Java and you wish to participate to development of Web 2.0 sites using the best frameworks available.');
-        $jobSensioLabs->setHowToApply('Send your resume to fabien.potencier [at] sensio.com');
-        $jobSensioLabs->setIsPublic(true);
-        $jobSensioLabs->setIsActivated(true);
-        $jobSensioLabs->setToken('job_sensio_labs');
-        $jobSensioLabs->setEmail('job@example.com');
-        $jobSensioLabs->setExpiresAt(new \DateTime('+30 days'));
+        $faker = Factory::create();
+        for ($i=0; $i<10; $i++){
+            $job = new Job();
+            $job->setCategory($this->getReference($faker->randomElement([
+                CategoryFixtures::CATEGORY_PROGRAMMING,
+                CategoryFixtures::CATEGORY_DESIGN,
+                CategoryFixtures::CATEGORY_MANAGER,
+            ])));
+            $job->setType($faker->randomElement(self::JOB_TYPE));
+            $job->setCompany($faker->company);
+            $job->setLogo($faker->imageUrl());
+            $job->setUrl($faker->url);
+            $job->setPosition($faker->jobTitle);
+            $job->setLocation($faker->country);
+            $job->setDescription($faker->text(1000));
+            $job->setHowToApply($faker->paragraph);
+            $job->setIsPublic($faker->boolean(50));
+            $job->setIsActivated($faker->boolean(50));
+            $job->setToken($faker->uuid);
+            $job->setEmail($faker->email);
+            $job->setExpiresAt($faker->dateTimeBetween('-1 year', '+1 year'));
 
-        $jobExtremeSensio = new Job();
-        $jobExtremeSensio->setCategory($this->getReference(CategoryFixtures::CATEGORY_DESIGN));
-        $jobExtremeSensio->setType('part-time');
-        $jobExtremeSensio->setCompany('Extreme Sensio');
-        $jobExtremeSensio->setLogo('extreme-sensio.gif');
-        $jobExtremeSensio->setUrl('http://www.extreme-sensio.com/');
-        $jobExtremeSensio->setPosition('Web Designer');
-        $jobExtremeSensio->setLocation('Paris, France');
-        $jobExtremeSensio->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.');
-        $jobExtremeSensio->setHowToApply('Send your resume to fabien.potencier [at] sensio.com');
-        $jobExtremeSensio->setIsPublic(true);
-        $jobExtremeSensio->setIsActivated(true);
-        $jobExtremeSensio->setToken('job_extreme_sensio');
-        $jobExtremeSensio->setEmail('job@example.com');
-        $jobExtremeSensio->setExpiresAt(new \DateTime('+30 days'));
-
-        $manager->persist($jobSensioLabs);
-        $manager->persist($jobExtremeSensio);
+            $manager->persist($job);
+        }
 
         $manager->flush();
     }
 
-    public function getDependencies()
-    {
-        return [
-          CategoryFixtures::class
-        ];
-    }
+        public function getDependencies()
+        {
+            return [
+              CategoryFixtures::class
+            ];
+        }
 }
